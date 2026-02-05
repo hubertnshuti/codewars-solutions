f119152a328055e1567bd753d0cdc0c4744f45e5
Hubert <72660209+hubertnshuti@users.noreply.github.com>
2026-02-05 06:49:58 +0200

    Add solution - CodeHub

---
+++ 7-kyu/jaden-casing-strings/jaden-casing-strings.ts
@@ -0,0 +1,11 @@
+String.prototype.toJadenCase = function (): string {
+  return this
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+};
+​
+interface String {      // Declaration needed, don't remove it
+  toJadenCase(): string;
+}
+​
