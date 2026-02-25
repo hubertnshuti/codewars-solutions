4b42b3d10ea59a2f275febd1bcc3391f201221d8
Hubert <72660209+hubertnshuti@users.noreply.github.com>
2026-02-25 23:58:30 +0200

    Add solution - CodeHub

---
+++ 7-kyu/is-sator-square/is-sator-square.ts
@@ -0,0 +1,16 @@
+export function isSatorSquare(tablet: string[][]): boolean {
+  const n = tablet.length;
+​
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (
+        tablet[i][j] !== tablet[j][i] ||
+        tablet[i][j] !== tablet[n - 1 - i][n - 1 - j]
+      ) {
+        return false;
+      }
+    }
+  }
+​
+  return true;
+}
