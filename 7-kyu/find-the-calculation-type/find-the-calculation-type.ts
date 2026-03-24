fdcee31a00167bb22e350dd44739caf4f8b1085a
Hubert <72660209+hubertnshuti@users.noreply.github.com>
2026-03-24 23:56:18 +0200

    Add solution - CodeHub

---
+++ 7-kyu/find-the-calculation-type/find-the-calculation-type.ts
@@ -0,0 +1,8 @@
+export function calcType(a: number, b: number, res: number): string {
+  if (a + b === res) return "addition";
+  if (a - b === res) return "subtraction";
+  if (a * b === res) return "multiplication";
+  if (a / b === res) return "division";
+  
+  return ""; // just fallback (won’t be used)
+}
