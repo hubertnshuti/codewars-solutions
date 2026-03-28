65baee15044055b69ca948555b82a9853c0a3446
Hubert <72660209+hubertnshuti@users.noreply.github.com>
2026-03-28 23:15:27 +0200

    Add solution - CodeHub

---
+++ 7-kyu/ninja-vs-samurai-strike/ninja-vs-samurai-strike.ts
@@ -0,0 +1,13 @@
+export class Warrior {
+  private name: string;
+  public health: number;
+​
+  constructor(name: string) {
+    this.name = name;
+    this.health = 100;
+  }
+​
+  strike(enemy: Warrior, swings: number): void {
+    enemy.health = Math.max(0, enemy.health - swings * 10);
+  }
+}
