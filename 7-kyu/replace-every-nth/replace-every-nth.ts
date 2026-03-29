bbebb67de6e7897455b753f74a21ffc374cd0148
Hubert <72660209+hubertnshuti@users.noreply.github.com>
2026-03-29 23:56:39 +0200

    Add solution - CodeHub

---
+++ 7-kyu/replace-every-nth/replace-every-nth.ts
@@ -0,0 +1,18 @@
+export function replaceNth(text: string, n: number, oldValue: string, newValue: string): string {
+  if (n <= 0) return text;
+  let count = 0;
+  let result = '';
+​
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === oldValue) {
+      count++;
+      if (count % n === 0) {
+        result += newValue;
+        continue;
+      }
+    }
+    result += text[i];
+  }
+​
+  return count < n ? text : result;
+}
